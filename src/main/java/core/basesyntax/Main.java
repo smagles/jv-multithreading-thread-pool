e3e38c19c01bf6da270d225751bb072ee079dd5f
smagles <lesialist@icloud.com>
2025-08-04 19:33:02 +0200

    Implement tread pool with 20 task and log execution results

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +15,23 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            Callable<String> callable = new MyThread();
+            Future<String> future = executorService.submit(callable);
+            futures.add(future);
+        }
+
+        try {
+            for (Future<String> future : futures) {
+                String value = future.get();
+                logger.info(value);
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+
+        }
+        executorService.shutdown();
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,20 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int duration = random.nextInt(1000);
+        Thread.sleep(duration);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSS");
+        return "Task duration was "
+                + duration + " ms, execution finished at "
+                + LocalTime.now().format(formatter);
     }
 }
